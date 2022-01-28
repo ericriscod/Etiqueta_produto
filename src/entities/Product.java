1b6f18b78f5e0ddbb7d758addf2dff4b262cb43c
ericriscod <rossato.ep@gmail.com>
2022-01-27 22:26:09 -0300

    Projeto finalizado

---
+++ src/entities/Product.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Product {
+
+	protected String name;
+	protected Double price;
+
+	public Product() {
+	}
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String priceTag() {	
+		return name + " $" + String.format("%.2f", price);
+	}
+	
+}
